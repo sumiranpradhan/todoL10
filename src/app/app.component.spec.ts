dd95620de84032e13978286ca0dd02a700bb07cc
Sumiran <43204798+antihero008@users.noreply.github.com>
2020-11-04 14:11:02 +0530

    First Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todoL10'`, () => {
+  it(`should have as title 'todoApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todoL10');
+    expect(app.title).toEqual('todoApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('todoL10 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('todoApp app is running!');
   });
 });
